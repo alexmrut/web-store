7ffe636b9173c723f8b8cc02a62c7261a0f314ec
alexmrut <alexandr.mrut@gmail.com>
2021-10-30 01:02:08 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//git init    init new empty repository
+//git commit -m "init commit"
+//git remote add origin git@github.com:alexmrut/web-store.git - link the project to git repository
+//git add -A      add all files
+
+
+import './App.css';
+import './homepage.styles.scss';
+import React from 'react';
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div >
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
